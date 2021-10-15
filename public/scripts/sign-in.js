4cc3a1e105cf3d3c06d02f17a0ff648019975b68
Kevin Long <kevin.uu.long@gmail.com>
2021-10-15 18:11:42 -0400

    Set auth token expiry for 3 months
    May change this duration at some point, 3 months was just a relatively arbitrary number that seemed about right

--- public/scripts/sign-in.js
+++ public/scripts/sign-in.js
@@ -23,7 +23,8 @@ document.querySelector("#authKey").addEventListener('keydown', (e) => {
 async function signIn() {
     var authKey = document.querySelector("#authKey").value;
     if (authKey != "") {
-        document.cookie = `authToken=${authKey};path=/`;
+        // Auth Token expires after 3 months:
+        document.cookie = `authToken=${authKey};path=/;max-age=${60*60*24*30*3}`;
         if (await authenticate()) {
             window.location = '/p/dashboard';
         }
